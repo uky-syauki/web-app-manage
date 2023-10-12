831bc3f6969a58d8a6cba478bfa477acaa92dc08
uky-syauki <achmad.uky@gmail.com>
2023-10-12 08:47:09 +0000

    1 server

---
+++ coconut.py
@@ -0,0 +1,8 @@
+from app import app, socketio
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=8000)
+
+# if __name__=='__main__':
+#     socketio.run(app)
+#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
